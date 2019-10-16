048d54377190c7fb88e0207eb08a0ee55bb75ab8
David Ellis <ducksual@gmail.com>
2019-10-16 13:53:18 +0100

    Black reformat

--- setup.py
+++ setup.py
@@ -1,34 +1,25 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.1.2'
-__author__ = 'DavidCEllis'
+__version__ = "0.1.2"
+__author__ = "DavidCEllis"
 
 
 setup(
-    name='splitnotes2',
+    name="splitnotes2",
     version=__version__,
-
-    packages=find_packages('src'),
-    url='',
-    license='GPLv3',
-    description='Quick tool for handling SEG-Y metadata and geometry.',
-    python_requires='>=3.6',
+    packages=find_packages("src"),
+    url="",
+    license="GPLv3",
+    description="Quick tool for handling SEG-Y metadata and geometry.",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 1 - Early Development',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 1 - Early Development",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
     ],
     package_dir={"": "src"},
-    install_requires=[
-        'pyside2',
-        'jinja2',
-    ],
-    tests_require=[
-        'pytest',
-        'pytest-cov',
-    ],
-    extras_require={
-        'build_exe': ['cx-freeze'],
-    }
+    install_requires=["pyside2", "jinja2"],
+    tests_require=["pytest", "pytest-cov"],
+    extras_require={"build_exe": ["cx-freeze"], "dev": ["black"]},
 )
