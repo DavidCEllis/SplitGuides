1728cc399431e22978bf2f424ed57f8acb6b561c
David Ellis <ducksual@gmail.com>
2019-10-16 17:04:16 +0100

    Formatting

--- tests/test_livesplit_client/test_get_client.py
+++ tests/test_livesplit_client/test_get_client.py
@@ -2,7 +2,7 @@ from splitnotes2.livesplit_client import get_client
 
 
 def test_get_client():
-    client = get_client('servername', 12, 2)
+    client = get_client("servername", 12, 2)
     assert client.connection.server == "servername"
     assert client.connection.port == 12
     assert client.connection.timeout == 2
