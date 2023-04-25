652938ba865854f6aa4fcaab672c672a37103286
David C Ellis <ducksual@gmail.com>
2023-04-25 10:01:46 +0100

    Handle errors, waitress code potentially in place.

--- application/app_server.py
+++ application/app_server.py
@@ -1,16 +1,19 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
-import flask.cli as cli
+# import waitress
+
+from flask import cli
 
 from splitguides.server import app, get_notes, settings
 from splitguides.ui.server_settings_ui import ServerSettingsDialog
 
 
-# Stop flask from giving users an unhelpful warning.
+# Suppress some of flask's messages
 cli.show_server_banner = lambda *x: None
 
 
 def launch():
-    base_app = QApplication()
+    # Create a base application and main window for the dialogs to use as parent
+    qt_app = QApplication()
     main_window = QMainWindow()
 
     settings_dialog = ServerSettingsDialog(
@@ -19,30 +22,48 @@ def launch():
     )
 
     result = settings_dialog.exec()
-
     if result == 0:  # Rejected, close without launching server
         print("Settings cancelled, closing application.")
+        qt_app.quit()
         return
 
-    get_notes()  # Sets internal 'notes' and 'notefile' variables
-
-    base_app.quit()
+    success = get_notes(main_window)  # Sets internal 'notes' and 'notefile' variables
+    if not success:
+        print("No notes file selected, closing application.")
+        qt_app.quit()
+        return
 
     print(
         "This server version of SplitGuides allows you view notes via a browser window "
         "and should work across a local network.\n"
-        "It is not intended to be used over the internet and as such is not based on a "
-        "production server."
+        "This uses a development server and is not intended "
+        "to be used over the internet."
     )
 
     print(
-        f"Connect a browser to http://{settings.server_hostname}:{settings.server_port}/ "
+        f"Connect a browser to "
+        f"http://{settings.server_hostname}:{settings.server_port}/ "
         f"in order to view the notes."
     )
 
-    print(f"This hostname and port can be changed in {settings.output_file} if needed.")
+    print("Press ctrl+c to close the server.")
 
-    app.run(threaded=True, host=settings.server_hostname, port=settings.server_port)
+    try:
+        app.run(
+            threaded=True,
+            host=settings.server_hostname,
+            port=settings.server_port
+        )
+
+        # waitress.serve(
+        #     app,
+        #     host=settings.server_hostname,
+        #     port=settings.server_port
+        # )
+    except KeyboardInterrupt:
+        print("Interrupt received, closing application.")
+    finally:
+        qt_app.quit()
 
 
 if __name__ == "__main__":
