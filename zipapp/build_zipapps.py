57c478e733b244ef6b7a710f70919ffbdfb51db3
David C Ellis <ducksual@gmail.com>
2024-09-27 14:32:17 +0100

    Commit the 0.9.1 lockfile and zipapp building scripts

--- zipapp/build_zipapps.py
+++ zipapp/build_zipapps.py
@@ -2,18 +2,46 @@ import sys
 from subprocess import run
 from pathlib import Path
 
+import splitguides
+from splitguides.build_ui import build_ui
 
 desktop_script = str(Path(__file__).parent / "splitguides_desktop.py")
 server_script = str(Path(__file__).parent / "splitguides_server.py")
 
+lockfile_name = f"splitguides_v{splitguides.__version__}.lock"
+lockfile_path = Path(__file__).parent / lockfile_name
+
+if not lockfile_path.exists():
+    print("LOCKFILE NOT GENERATED FOR THIS VERSION OF SPLITGUIDES - EXITING")
+    sys.exit()
+
+# The built UI files *must* be included - should already exist
+build_ui(replace=False)
+
 print("Building desktop zipapp")
 run(
-    [sys.executable, "-m", "ducktools.env", "bundle", str(desktop_script)],
+    [
+        sys.executable,
+        "-m",
+        "ducktools.env",
+        "bundle",
+        "--with-lock",
+        str(lockfile_path),
+        str(desktop_script),
+    ],
     check=True,
 )
 
 print("Building server zipapp")
 run(
-    [sys.executable, "-m", "ducktools.env", "bundle", str(desktop_script)],
+    [
+        sys.executable,
+        "-m",
+        "ducktools.env",
+        "bundle",
+        "--with-lock",
+        str(lockfile_path),
+        str(server_script),
+    ],
     check=True,
 )
