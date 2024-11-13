d906caeed3ef5ce2434d41591b10c0389cc97757
David C Ellis <ducksual@gmail.com>
2024-11-13 16:23:59 +0000

    Bump dependencies and version numbers and add a new lockfile

--- zipapp/splitguides_server.py
+++ zipapp/splitguides_server.py
@@ -31,7 +31,7 @@
 # [tool.ducktools.env.app]
 # name = "splitguides"
 # owner = "davidcellis"
-# version = "0.9.4"
+# version = "0.9.5"
 #
 # [tool.ducktools.env.include]
 # data = ["../src/splitguides", "../resources"]
