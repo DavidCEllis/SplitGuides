50a3a9b2266e2ae3fbb490ec2f0c925286eb7f03
Alex Knauth <alexander@knauth.org>
2025-06-04 15:07:19 -0400

    rename: rgba_to_qcolor, qcolor_to_rgba

--- src/splitguides/ui/color.py
+++ src/splitguides/ui/color.py
@@ -1,14 +1,14 @@
 from PySide6.QtGui import QColor
 import re
 
-def colorFromHexRgba(s: str):
+def rgba_to_qcolor(s: str):
     m = re.search(r"#(......)(..)", s)
     if m:
         return QColor("#" + m.group(2) + m.group(1))
     else:
         return QColor(s)
 
-def colorToHexRgba(color: QColor):
+def qcolor_to_rgba(color: QColor):
     argb = color.name(QColor.HexArgb)
     m = re.search(r"#(..)(......)", argb)
     return "#" + m.group(2) + m.group(1)
