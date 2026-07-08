c23519ee4e132ca99543f10d526b4dca267c83ee
Michael Kirsch <mkirsch.git@gmail.com>
2026-07-08 23:10:19 +0200

    Adapt tests

--- tests/test_livesplit_client/test_connection_tcp.py
+++ tests/test_livesplit_client/test_connection_tcp.py
@@ -3,8 +3,7 @@ from unittest.mock import patch, MagicMock
 
 import pytest
 
-from splitguides.livesplit_client import ConnectionTCP
-from splitguides.livesplit_client.connection_shared import BUFFER_SIZE
+from splitguides.livesplit_client.connection_shared import ConnectionTCP, BUFFER_SIZE
 
 
 def test_init():
@@ -16,9 +15,11 @@ def test_init():
 
 
 def test_connect():
-    with patch("socket.socket") as mock_socket:
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
 
         connection = ConnectionTCP(server="host", port=12, timeout=2)
         response = connection.connect()
@@ -27,13 +28,17 @@ def test_connect():
         mock_sock.connect.assert_called_with(("host", 12))
         mock_sock.settimeout.assert_called_with(2)
 
+        mock_ping.assert_called_once()
+        mock_ping.assert_called_with(mock_sock)
+
         assert response is True
 
-
 def test_failed_connect():
-    with patch("socket.socket") as mock_socket:
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
 
         mock_sock.connect.side_effect = ConnectionRefusedError("Should be handled")
         connection = ConnectionTCP()
@@ -42,11 +47,26 @@ def test_failed_connect():
 
         assert response is False
 
-
-def test_close():
+def test_connected_to_websocket():
     with patch("socket.socket") as mock_socket:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
+        mock_sock.recv.side_effect = TimeoutError("Timeout on TCP connection to Websocket")
+
+        connection = ConnectionTCP(server="host", port=12, timeout=2)
+        response = connection.connect()
+
+        mock_socket.assert_called_once()
+        mock_sock.connect.assert_called_with(("host", 12))
+
+        assert response is False
+
+def test_close():
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
+        mock_sock = MagicMock()
+        mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
 
         connection = ConnectionTCP()
         connection.connect()
@@ -61,53 +81,52 @@ def test_close():
         mock_sock.close.assert_not_called()
 
 
-@pytest.mark.parametrize("connect_first", [True, False])
-def test_send(connect_first):
-    with patch("socket.socket") as mock_socket:
+def test_send():
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
 
+        mock_ping.return_value = True
+
         connection = ConnectionTCP()
 
-        if connect_first:
-            connection.connect()
+        connection.connect()
 
         connection.send(b"test message")
 
         mock_socket.assert_called_once()
         mock_sock.connect.assert_called_with(("localhost", 16834))
-        mock_sock.send.assert_called_with(b"test message")
+        mock_sock.send.assert_called_with(b"test message\r\n")
 
 
 def test_send_fail():
-    with patch("socket.socket") as mock_socket:
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
-        mock_sock.send.side_effect = ConnectionAbortedError("Should be caught")
+        mock_sock.send.side_effect = ConnectionError("Should be caught")
+
+        mock_ping.return_value = True
 
         connection = ConnectionTCP()
 
-        with pytest.raises(ConnectionAbortedError):
+        with pytest.raises(ConnectionError):
             connection.send(b"test message")
 
-        mock_sock.send.assert_called_with(b"test message")
-
-        mock_sock.close.assert_called_once()
         assert connection.sock is None
 
 
-@pytest.mark.parametrize("connect_first", [True, False])
-def test_receive(connect_first):
-    with patch("socket.socket") as mock_socket:
+def test_receive():
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
 
         mock_sock.recv.return_value = b"returned data"
 
         connection = ConnectionTCP()
 
-        if connect_first:
-            connection.connect()
+        connection.connect()
 
         result = connection.receive()
 
@@ -117,9 +136,11 @@ def test_receive(connect_first):
 
 
 def test_receive_empty():
-    with patch("socket.socket") as mock_socket:
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
 
         mock_sock.recv.return_value = b""
 
@@ -128,18 +149,20 @@ def test_receive_empty():
         with pytest.raises(ConnectionError):
             connection.receive()
 
-        mock_sock.close.assert_called_once()
         assert connection.sock is None
 
 
 def test_receive_timeout():
-    with patch("socket.socket") as mock_socket:
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
 
         mock_sock.recv.side_effect = socket.timeout()
 
         connection = ConnectionTCP()
+        connection.connect()
 
         with pytest.raises(TimeoutError):
             connection.receive()
@@ -149,13 +172,34 @@ def test_receive_timeout():
 
 
 def test_receive_oserror():
-    with patch("socket.socket") as mock_socket:
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
         mock_sock = MagicMock()
         mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
 
         mock_sock.recv.side_effect = OSError("Confusing windows message.")
 
         connection = ConnectionTCP()
+        connection.connect()
+
+        with pytest.raises(ConnectionError):
+            connection.receive()
+
+        mock_sock.close.assert_called_once()
+        assert connection.sock is None
+
+def test_receive_server_closed():
+    with patch("socket.socket") as mock_socket, patch("splitguides.livesplit_client.connection_shared.ConnectionTCP._ping") as mock_ping:
+        mock_sock = MagicMock()
+        mock_socket.return_value = mock_sock
+
+        mock_ping.return_value = True
+
+        mock_sock.recv.return_value = b""
+
+        connection = ConnectionTCP()
+        connection.connect()
 
         with pytest.raises(ConnectionError):
             connection.receive()
